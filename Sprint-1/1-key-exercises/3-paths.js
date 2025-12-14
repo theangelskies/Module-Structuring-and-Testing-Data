eb358d1e1bba7ed1850801bc9e6b53fb488bfbc9
theangelskies <theangelskies13@yahoo.com>
2025-12-14 23:06:30 +0100

    Implement extraction of directory and extension from file path

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,9 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex);
+const ext = filePath.slice(filePath.lastIndexOf("."));
+
+
 
 // https://www.google.com/search?q=slice+mdn
