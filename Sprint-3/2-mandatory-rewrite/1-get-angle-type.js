fa6de350b5ddc9cc06ef432dff7e7236762198fd
theangelskies <theangelskies13@yahoo.com>
2025-12-22 01:35:46 +0100

    Refactor getAngleType function and enhance test cases for angle classification

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,7 +1,14 @@
+// function getAngleType(angle) {
+//   if (angle === 90) return "Right angle";
+//   // replace with your completed function from key-implement
+
+// }
 function getAngleType(angle) {
   if (angle === 90) return "Right angle";
-  // replace with your completed function from key-implement
-
+  if (angle < 90) return "Acute angle";
+  if (angle > 90 && angle < 180) return "Obtuse angle";
+  if (angle === 180) return "Straight angle";
+  if (angle > 180 && angle < 360) return "Reflex angle";
 }
 
 
