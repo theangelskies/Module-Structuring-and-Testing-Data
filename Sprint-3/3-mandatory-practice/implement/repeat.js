b350d8eeae17c3df8f0f14b24c592d55e21ebd22
theangelskies <theangelskies13@yahoo.com>
2025-12-25 11:51:14 +0100

    Implement repeat function to concatenate a string a specified number of times with error handling for negative counts

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,15 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (count < 0) {
+    throw new Error("Count must be a non-negative integer");
+  }
+
+  let result = "";
+
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+
+  return result;
 }
 
 module.exports = repeat;
