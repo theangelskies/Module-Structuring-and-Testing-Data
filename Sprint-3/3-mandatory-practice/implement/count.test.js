40f53fc3610eb2a4cb6d4e5a06a62b39d283acc3
theangelskies <theangelskies13@yahoo.com>
2025-12-25 11:15:12 +0100

    Implement countChar function and add test case for no occurrences scenario

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -18,28 +18,15 @@ test("should count multiple occurrences of a character", () => {
 });
 
 // Scenario: No Occurrences
-// test("should return 0 when character does not occur", () => {
-//   const str = "hello";
-//   const char = "z";
-//   const count = countChar(str, char);
-//   expect(count).toEqual(0);
-// });
-function countChar(stringOfCharacters, findCharacter) {
-  let count = 0;
-
-  for (let i = 0; i < stringOfCharacters.length; i++) {
-    if (stringOfCharacters[i] === findCharacter) {
-      count++;
-    }
-  }
-
-  return count;
-}
-
-module.exports = countChar;
-
-
 // Given the input string str,
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+
+test("should return 0 when character does not occur", () => {
+  const str = "hello";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
