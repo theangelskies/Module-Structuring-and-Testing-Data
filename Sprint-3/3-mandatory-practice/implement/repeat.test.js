7b61eab3620400ed824fc21f82f6f607a2d11ad4
theangelskies <theangelskies13@yahoo.com>
2025-12-25 11:56:58 +0100

    Add test case for handling count of 0 in repeat function

--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -28,6 +28,9 @@ test("should return the original string when count is 1", () => {
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
+test("should return an empty string when count is 0", () => {
+  expect(repeat("hello", 0)).toEqual("");
+});
 
 // case: Negative Count:
 // Given a target string str and a negative integer count,
