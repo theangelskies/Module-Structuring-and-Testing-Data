fe13eeba114b5b573c49783ee5c128ad439cc336
theangelskies <theangelskies13@yahoo.com>
2025-12-25 15:57:12 +0100

    Add credit card validation function with test cases

--- Sprint-3/4-stretch-investigate/creditCardValidator.js
+++ Sprint-3/4-stretch-investigate/creditCardValidator.js
@@ -2,3 +2,21 @@
 // @param {string} cardNumber - The credit card number to validate
 // @returns {boolean} - true if valid, false if invalid
  
+function isValidCreditCard(cardNumber) {
+  // Rule 1: Must be exactly 16 digits and contain only numbers
+  if (!/^\d{16}$/.test(cardNumber)) {
+    return false;
+  }
+
+  // If all rules pass, the credit card number is valid
+  return true;
+}
+
+
+
+  /* ---------- Test cases ---------- */
+console.log(isValidCreditCard("9999777788880000")); // true
+console.log(isValidCreditCard("999777788880000")); // false (15 digits)
+
+
+module.exports = isValidCreditCard;
