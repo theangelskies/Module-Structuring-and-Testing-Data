c8452f6981999cca169b22e488ecbf700404c841
theangelskies <theangelskies13@yahoo.com>
2025-12-25 15:49:00 +0100

    Add credit card validation function with parameter documentation

---
+++ Sprint-3/4-stretch-investigate/creditCardValidator.js
@@ -0,0 +1,4 @@
+// Validates whether a credit card number meets specific rules
+// @param {string} cardNumber - The credit card number to validate
+// @returns {boolean} - true if valid, false if invalid
+ 
