b610f41fe39794b891ce1442339e02dea59e0410
theangelskies <theangelskies13@yahoo.com>
2026-01-06 14:15:28 +0100

    dir adjust

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = filePath.slice(0, lastSlashIndex);
+const dir = filePath.slice(1, lastSlashIndex);
 const ext = filePath.slice(filePath.lastIndexOf("."));
 
 
