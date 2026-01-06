8c49a7d033fdf0dcbfbf932d8085f97a7ae58d1d
theangelskies <theangelskies13@yahoo.com>
2026-01-06 14:01:57 +0100

    log added

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -6,6 +6,8 @@ let lastName = "Johnson";
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
 let initials = firstName[0] + middleName[0] + lastName[0];
+console.log(initials);
+
 
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
