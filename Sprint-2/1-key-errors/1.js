f65331ae279add71c5834f067993a1f87a62399d
theangelskies <157623774+theangelskies@users.noreply.github.com>
2026-01-06 19:48:53 +0100

    Update Sprint-2/1-key-errors/1.js
    Co-authored-by: Paweł Szymański <pawelfiu@gmail.com>

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -21,8 +21,7 @@
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 function convertToPercentage(decimalNumber) {
-  const percentage = `${decimalNumber * 100}%`;
-  return percentage;
+  return `${decimalNumber * 100}%`;
 }
 
 const result = convertToPercentage(0.5);
